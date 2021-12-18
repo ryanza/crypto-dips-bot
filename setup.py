7e5a5096185c1e69beb13cced26de1089c32a950
omaral92 <omaral92@gmail.com>
2021-12-18 00:06:12 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='packages',
+      version='0.1',
+      author='omaral92',
+      author_email='omaral92@gmail.com',
+      packages=['packages'],
+      zip_safe=False)
